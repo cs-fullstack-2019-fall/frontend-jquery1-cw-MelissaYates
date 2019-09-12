ceb1ef4f881b680954b9573aa29ca7d33bbd3ecf
Melissa Yates <melissampeart@gmail.com>
2019-09-12 12:52:22 -0500

    tried to get it

---
+++ exercise2.js
@@ -0,0 +1,13 @@
+
+
+let is_image_visible = true;
+
+if(is_image_visible === true){
+    $("#block").show();
+    setInterval(function() { $("#block").hide(); }, 2000);
+}
+else{
+    $("#block").hide();
+    setTimeout(function() { $("#block").show(); }, 2000);
+}
+/
---
+++ index1.js
@@ -0,0 +1,7 @@
+$("div .redText").css("redText", "red");
+$("#myElement").addClass("blue");
+$("#first").css("background-color","green", 'color','white');
+$("#second").css("background-color","orange",'color', 'pink');
+$('#myElement').on('click', function(event) {
+    alert("My Element was clicked!");
+});
