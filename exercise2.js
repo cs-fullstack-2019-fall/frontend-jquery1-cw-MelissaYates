8ead1210b192729e92dd276f3faf384723cc21af
autumn-ragland <raglanda998@gmail.com>
2019-09-12 20:27:27 -0500

    Graded 09-12-2019 20:25:14

--- exercise2.js
+++ exercise2.js
@@ -5,8 +5,10 @@ let is_image_visible = true;
 if(is_image_visible === true){
     $("#block").show();
     setInterval(function() { $("#block").hide(); }, 2000);
+    // !! : update the value of your flag 
 }
 else{
     $("#block").hide();
     setTimeout(function() { $("#block").show(); }, 2000);
+        // !! : update the value of your flag 
 }
